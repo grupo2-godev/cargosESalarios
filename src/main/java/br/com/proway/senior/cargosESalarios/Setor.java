85747d26969d7031905e4ad175857184949d21ee
SENIOR <SENIOR@proway.treina>
2021-04-16 14:09:34 -0300

    Getters and setters

--- src/main/java/br/com/proway/senior/cargosESalarios/Setor.java
+++ src/main/java/br/com/proway/senior/cargosESalarios/Setor.java
@@ -6,4 +6,74 @@ public class Setor {
 	String nomeSetor;
 	int capacidade;
 	int idPermissao;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nomeSetor
+	 */
+	public String getNomeSetor() {
+		return nomeSetor;
+	}
+	/**
+	 * @param nomeSetor the nomeSetor to set
+	 */
+	public void setNomeSetor(String nomeSetor) {
+		this.nomeSetor = nomeSetor;
+	}
+	/**
+	 * @return the capacidade
+	 */
+	public int getCapacidade() {
+		return capacidade;
+	}
+	/**
+	 * @param capacidade the capacidade to set
+	 */
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+	/**
+	 * @return the idPermissao
+	 */
+	public int getIdPermissao() {
+		return idPermissao;
+	}
+	/**
+	 * @param idPermissao the idPermissao to set
+	 */
+	public void setIdPermissao(int idPermissao) {
+		this.idPermissao = idPermissao;
+	}
+	/**
+	 * @param id
+	 * @param nomeSetor
+	 * @param capacidade
+	 * @param idPermissao
+	 */
+	public Setor(int id, String nomeSetor, int capacidade, int idPermissao) {
+		super();
+		this.id = id;
+		this.nomeSetor = nomeSetor;
+		this.capacidade = capacidade;
+		this.idPermissao = idPermissao;
+	}
+	/**
+	 * 
+	 */
+	public Setor() {
+		super();
+	}
+	
+	
+	
 }
