8921a9699f10e14f7e153741d0a1e850487dcad8
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-11 15:15:40 -0300

    Documentação ConnectionMySQL

--- src/main/java/br/com/proway/senior/cargosESalarios/connection/IConectar.java
+++ src/main/java/br/com/proway/senior/cargosESalarios/connection/IConectar.java
@@ -5,8 +5,9 @@ import java.sql.Connection;
 /**
  * Interface IConectar
  * 
- * Dita o comportamento das classes ConnectionPostgres {@link ConnetionPostgres} e ConnectionMySQL
- * {@link ConnectionMySQL}. Ambas serao etapas da FactoryConexao {@link FactoryConexao}.
+ * Dita o comportamento das classes ConnectionPostgres {@link ConnetionPostgres}
+ * e ConnectionMySQL {@link ConnectionMySQL}. Ambas serao etapas da
+ * FactoryConexao {@link FactoryConexao}.
  * 
  * @author Sarah Brito <b>sarah.brito@senior.com.br</b> - Sprint 5
  *
@@ -17,11 +18,11 @@ public interface IConectar {
 	/**
 	 * Metodo conectar().
 	 * 
-	 * Apos implementacao da Interface, recebe os parametros de URL, user e password para conectar ao
-	 * banco de dados escolhido. Retorna uma conexao.
+	 * Apos implementacao da Interface, recebe os parametros de URL, user e password
+	 * para conectar ao banco de dados escolhido. Retorna uma conexao.
 	 * 
 	 * @return Connection
 	 */
 	public Connection conectar();
-	
+
 }
