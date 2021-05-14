b1b0ce5e967d6f98bbdbacbcd6b18579c9129b65
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-14 08:56:27 -0300

    Métodos getInstance e contrututor do DAO crriados

--- src/main/java/br/com/proway/senior/cargosESalarios/model/DaoSQL/PostoDeTrabalhoDAO.java
+++ src/main/java/br/com/proway/senior/cargosESalarios/model/DaoSQL/PostoDeTrabalhoDAO.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import org.hibernate.Session;
+
 import br.com.proway.senior.cargosESalarios.connection.antigo.ConnectionPostgres;
 import br.com.proway.senior.cargosESalarios.connection.antigo.FactoryConexao;
 import br.com.proway.senior.cargosESalarios.connection.antigo.FactoryPostgres;
@@ -13,20 +15,44 @@ import br.com.proway.senior.cargosESalarios.model.PostoDeTrabalhoModel;
 import br.com.proway.senior.cargosESalarios.model.Interface.InterfaceDAOCRUD;
 
 /**
- * Classe PostoDeTrabalhoDaoSql
+ * Classe PostoDeTrabalhoDAO
  * 
- * Classe DAO que implementa a interface CRUDInterface para
- * interacao com o banco de dados.
+ * Classe DAO que implementa a interface InterfaceDAOCRUD para interacao com o
+ * banco de dados.
  * 
- * @author Sarah Brito, sarah.brito@senior.com.br
- * @author Lorran Santos, lorran.santos@senior.com.br
- * @author Samuel Levi, samuel.levi@senior.com.br
+ * @author Sarah Brito <b>sarah.brito@senior.com.br</b> - Sprint 4 e 5
+ * @author Lorran Santos, lorran.santos@senior.com.br - Sprint 4
+ * @author Samuel Levi, samuel.levi@senior.com.br - Sprint 4
  */
 
 public class PostoDeTrabalhoDAO implements InterfaceDAOCRUD<PostoDeTrabalhoModel> {
+
+	private static PostoDeTrabalhoDAO instance;
+	private Session session;
 	
-	FactoryConexao conexao = new FactoryPostgres();
+	/**
+	 * Singleton da classe PostoDeTrabalhoDAO.
+	 * 
+	 * @param session
+	 * @return PostoDeTrabalhoDAO
+	 */
+	public static PostoDeTrabalhoDAO getInstance(Session session) {
+		if (instance == null) {
+			instance = new PostoDeTrabalhoDAO(session);
+		}
+		return instance;
+	}
 	
+	/**
+	 * Contrutor da classe PostoDeTrabalhoDAO, utilizado no Singleton.
+	 * 
+	 * @param Session session
+	 */
+	private PostoDeTrabalhoDAO(Session session) {
+		this.session = session;
+	}
+	
+
 	/***
 	 * Inserir Posto de Trabalho.
 	 * 
@@ -60,33 +86,12 @@ public class PostoDeTrabalhoDAO implements InterfaceDAOCRUD<PostoDeTrabalhoModel
 		}
 		return quantidadeRegistros;
 	}
-	
-	/**
-	 * Metodo countValores
-	 * 
-	 * Metodo realiza um select no banco para verificar a quantidade
-	 * de registros e retorna esse valor.
-	 * 
-	 * @return int quantidade
-	 */
-	public int countValores() {
-		int quantidade = 0;
-		try {
-			String sqlCount = "SELECT COUNT(*) FROM grupo2.posto_de_trabalho";
-			ResultSet rs = conexao.criarConexao().createStatement().executeQuery(sqlCount);
-			rs.next();
-			quantidade = rs.getInt(1);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return quantidade;
-	}
-	
+
 	/**
 	 * Metodo retrieve por idPosto
 	 * 
-	 * Metodo realiza a busca dos dados do posto no banco de dados,
-	 * conforme idPosto informada.
+	 * Metodo realiza a busca dos dados do posto no banco de dados, conforme idPosto
+	 * informada.
 	 * 
 	 * @param int idPosto
 	 * @return PostoDeTrabalhoModel
@@ -112,19 +117,18 @@ public class PostoDeTrabalhoDAO implements InterfaceDAOCRUD<PostoDeTrabalhoModel
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Metodo retrieve por nomePosto
 	 * 
-	 * Metodo realiza a busca dos dados do posto no banco de dados,
-	 * conforme nomePosto informado.
+	 * Metodo realiza a busca dos dados do posto no banco de dados, conforme
+	 * nomePosto informado.
 	 * 
 	 * @param String nomePosto
 	 * @return PostoDeTrabalhoModel
 	 */
 	public PostoDeTrabalhoModel retrieve(String nomePosto) {
-		String sqlRetrieveNome = "SELECT * FROM grupo2.posto_de_trabalho WHERE nome_posto = " 
-				+ "'" + nomePosto + "'";
+		String sqlRetrieveNome = "SELECT * FROM grupo2.posto_de_trabalho WHERE nome_posto = " + "'" + nomePosto + "'";
 		try {
 			Statement stmt = conexao.criarConexao().createStatement();
 			ResultSet rs = stmt.executeQuery(sqlRetrieveNome);
@@ -148,10 +152,10 @@ public class PostoDeTrabalhoDAO implements InterfaceDAOCRUD<PostoDeTrabalhoModel
 	/**
 	 * Metodo upadate
 	 * 
-	 * Metodo realiza a atualizacao dos dados no banco de dados
-	 * para o posto informado, conforme idPosto.
+	 * Metodo realiza a atualizacao dos dados no banco de dados para o posto
+	 * informado, conforme idPosto.
 	 * 
-	 * @param int idPosto
+	 * @param int                  idPosto
 	 * @param PostoDeTrabalhoModel postoModel
 	 * @return boolean
 	 */
@@ -177,8 +181,7 @@ public class PostoDeTrabalhoDAO implements InterfaceDAOCRUD<PostoDeTrabalhoModel
 	/**
 	 * Metodo delete
 	 * 
-	 * Realiza a exclus�o do posto de trabalho informado
-	 * em idPosto.
+	 * Realiza a exclus�o do posto de trabalho informado em idPosto.
 	 * 
 	 * @param int idPosto
 	 * @return boolean
@@ -191,24 +194,24 @@ public class PostoDeTrabalhoDAO implements InterfaceDAOCRUD<PostoDeTrabalhoModel
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}	
+		}
 		return false;
 	}
 
 	/**
 	 * Metodo getAll
 	 * 
-	 * Metodo realiza a busca de todos os postos de trabalho
-	 * cadastrados no banco e armazena em um ArrayList.
+	 * Metodo realiza a busca de todos os postos de trabalho cadastrados no banco e
+	 * armazena em um ArrayList.
 	 * 
 	 * @return ArrayList<PostoDeTrabalhoModel>
 	 */
 	public ArrayList<PostoDeTrabalhoModel> getAll() {
-        ArrayList<PostoDeTrabalhoModel> results = new ArrayList<PostoDeTrabalhoModel>();
-        String sqlSelectAll = "SELECT * FROM grupo2.posto_de_trabalho";
-        try {
-        	Statement stmt = conexao.criarConexao().createStatement();
-     		ResultSet rs = stmt.executeQuery(sqlSelectAll);
+		ArrayList<PostoDeTrabalhoModel> results = new ArrayList<PostoDeTrabalhoModel>();
+		String sqlSelectAll = "SELECT * FROM grupo2.posto_de_trabalho";
+		try {
+			Statement stmt = conexao.criarConexao().createStatement();
+			ResultSet rs = stmt.executeQuery(sqlSelectAll);
 			PostoDeTrabalhoModel posto = new PostoDeTrabalhoModel();
 			while (rs.next()) {
 				posto.setIdCargo(rs.getInt(1));
@@ -216,30 +219,36 @@ public class PostoDeTrabalhoDAO implements InterfaceDAOCRUD<PostoDeTrabalhoModel
 				posto.setIdSetor(rs.getInt(3));
 				posto.setNomePosto(rs.getString(4));
 				posto.setSalario(rs.getDouble(5));
-				posto.setIdPosto(rs.getInt(6)); 
+				posto.setIdPosto(rs.getInt(6));
 				System.out.println(posto.toString());
-               	results.add(posto);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return results;
+				results.add(posto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return results;
 	}
-	
+
+	public boolean deleteAll() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 	/**
 	 * Metodo limparTabela
 	 * 
 	 * Metodo realiza a limpeza da tabela no banco de dados, deletando os registros
-	 * e resetando a PrimaryKey. O foco eh ser utilizado nos testes. É necessário criar
-	 * as sequences no banco, pois eh partir delas que a Primary Key é resetada.
+	 * e resetando a PrimaryKey. O foco eh ser utilizado nos testes. É necessário
+	 * criar as sequences no banco, pois eh partir delas que a Primary Key é
+	 * resetada.
 	 * 
 	 * @throws SQLException
 	 * @return void
 	 */
 	public void limparTabela() throws SQLException {
 		String limpar = "delete from grupo2.posto_de_trabalho";
-		String removerIncremento = "ALTER SEQUENCE grupo2.posto_de_trabalho_increment RESTART";	
-		conexao.criarConexao().createStatement().executeUpdate(limpar);	
+		String removerIncremento = "ALTER SEQUENCE grupo2.posto_de_trabalho_increment RESTART";
+		conexao.criarConexao().createStatement().executeUpdate(limpar);
 		conexao.criarConexao().createStatement().executeUpdate(removerIncremento);
 		System.out.println("Limpeza realizada.");
 	}
