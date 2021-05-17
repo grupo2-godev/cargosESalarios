858c7685d03bbe19a73f189bef69f0c3d65772ff
Janaina Mai <janainamai@hotmail.com>
2021-05-17 10:21:09 -0300

    Refatorado testes para método cadastrar, implementado método cadastrar na classe CargoController.

--- src/main/java/br/com/proway/senior/cargosESalarios/controller/CargoController.java
+++ src/main/java/br/com/proway/senior/cargosESalarios/controller/CargoController.java
@@ -3,8 +3,12 @@
  */
 package br.com.proway.senior.cargosESalarios.controller;
 
+import java.time.LocalDateTime;
+
 import br.com.proway.senior.cargosESalarios.connection.ConnectionHibernate;
+import br.com.proway.senior.cargosESalarios.model.CargoModel;
 import br.com.proway.senior.cargosESalarios.model.DaoSQL.CargoDAO;
+import utils.Validators;
 
 /**
  * Controller que interage com o CargoDAO.
@@ -12,9 +16,75 @@ import br.com.proway.senior.cargosESalarios.model.DaoSQL.CargoDAO;
  * @author Janaina Mai <b>janaina.mai@senior.com.br</b> - Sprint 5
  *
  */
-public class CargoController {	
-	
+public class CargoController {
+
 	CargoDAO cargoDAO = CargoDAO.getInstance(ConnectionHibernate.getSession());
 
-	
+	/**
+	 * Cadastra um objeto do tipo {@link CargoModel} no banco de dados.
+	 * 
+	 * Valida se eh vazio ou nulo: nomeCargo, experienciaMinima, atribuicoes e
+	 * status. Valida se eh nullo ou igual a zero: cbo2002, cbo94, horasMes,
+	 * grauInstrucao, idPermissao.
+	 * 
+	 * @param nomeCargo         String Nome do cargo.
+	 * @param dataCadastro      LocalDateTime Data de cadastro do cargo.
+	 * @param dataUltimaRevisao LocalDateTime Data da ultima alteracao do cargo.
+	 * @param cbo2002           Integer Definicao CBO (Classificacao Brasileira de
+	 *                          Ocupacoes) de 2002.
+	 * @param cbo94             Integer Definicao CBO (Classificacao Brasileira de
+	 *                          Ocupacoes) de 1994.
+	 * @param horasMes          Integer Quantidade de horas trabalhada por mes.
+	 * @param grauInstrucao     Integer Id do {@link GrauInstrucaoModel}.
+	 * @param experienciaMinima Integer Experiencia minima em anos.
+	 * @param atribuicoes       String Descricao das atividades que serao
+	 *                          executadas.
+	 * @param status            Boolean Cargo ativo(true) ou inativo(false).
+	 * @param idPermissao       Integer Id do nivel de acesso permitido para esse
+	 *                          tipo de colaborador.
+	 *                          
+	 * @return Id do objeto cadastrado.
+	 * @throws Exception Retorna uma excessao caso algum dos valores recebidos no
+	 *                   parametro nao estejam de acordo conforme validacoes.
+	 */
+	public Integer cadastrar(String nomeCargo, LocalDateTime dataCadastro, LocalDateTime dataUltimaRevisao,
+			Integer cbo2002, Integer cbo94, Integer horasMes, Integer grauInstrucao, String experienciaMinima,
+			String atribuicoes, Boolean status, Integer idPermissao) throws Exception {
+
+		if (Validators.isNullObject(nomeCargo) || nomeCargo.isEmpty())
+			throw (new Exception("O nome do cargo não foi informado."));
+		if (Validators.isZeroOrNull(cbo2002))
+			throw (new Exception("O cbo2002 não foi informado."));
+		if (Validators.isZeroOrNull(cbo94))
+			throw (new Exception("O cbo94 não foi informado."));
+		if (Validators.isZeroOrNull(horasMes))
+			throw (new Exception("A quantidade de horas trabalhadas por mês não pode ser igual a zero."));
+		if (Validators.isZeroOrNull(grauInstrucao))
+			throw (new Exception("O grau de instrução não foi informado."));
+		if (Validators.isNullObject(experienciaMinima) || experienciaMinima.isEmpty())
+			throw (new Exception("A experiencia mínima não foi informada."));
+		if (Validators.isNullObject(atribuicoes) || atribuicoes.isEmpty())
+			throw (new Exception("As atribuicoes não foram informadas."));
+		if (Validators.isNullObject(status))
+			throw (new Exception("O status não foi informado."));
+		if (Validators.isZeroOrNull(idPermissao))
+			throw (new Exception("A permissao não foi informada."));
+
+		CargoModel cargo = new CargoModel();
+		cargo.setNomeCargo(nomeCargo);
+		cargo.setDataCadastro(dataCadastro);
+		cargo.setDataUltimaRevisao(dataUltimaRevisao);
+		cargo.setCbo2002(cbo2002);
+		cargo.setCbo94(cbo94);
+		cargo.setHoraMes(horasMes);
+		cargo.setGrauInstrucao(grauInstrucao);
+		cargo.setExperienciaMinima(experienciaMinima);
+		cargo.setAtribuicoes(atribuicoes);
+		cargo.setStatus(status);
+		cargo.setIdPermissao(idPermissao);
+
+		Integer idCargoCadastrado = cargoDAO.create(cargo);
+		return idCargoCadastrado;
+	}
+
 }
--- src/test/java/br/com/proway/senior/cargosESalarios/controller/CargoControllerTest.java
+++ src/test/java/br/com/proway/senior/cargosESalarios/controller/CargoControllerTest.java
@@ -1,6 +1,7 @@
 package br.com.proway.senior.cargosESalarios.controller;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
@@ -20,73 +21,92 @@ public class CargoControllerTest {
 
 	CargoController controller = new CargoController();
 
+	String nomeCargo = "Desenvolvedor 2";
+	LocalDateTime dataCadastro = LocalDateTime.now();
+	LocalDateTime dataUltimaRevisao = LocalDateTime.now();
+	Integer cbo2002 = 1;
+	Integer cbo94 = 1;
+	Integer horasMes = 220;
+	Integer grauInstrucao = 1;
+	String experienciaMinima = "1";
+	String atribuicoes = "Desenvolvedor";
+	Boolean status = true;
+	Integer idPermissao = 1;
+	
 	@Test
 	public void testCadastrar() {
 		assertEquals(0, controller.buscarTodos().size());
 
-		String nomeCargo = "Desenvolvedor 2";
-		LocalDateTime dataCadastro = LocalDateTime.now();
-		LocalDateTime dataUltimaRevisao = LocalDateTime.now();
-		Integer cbo2002 = 0;
-		Integer cbo94 = 0;
-		Integer horasMes = 220;
-		Integer grauInstrucao = 0;
-		String experienciaMinima = "1";
-		String atribuicoes = "Desenvolvedor";
-		Boolean status = true;
-		Integer idPermissao = 1;
-
-		assertTrue(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
+		assertNotNull(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
 				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao));
 		assertEquals(1, controller.buscarTodos().size());
-		controller.deletarTodos();
-
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarNomeInvalido() throws Exception {
 		String nomeCargoVazio = "";
-		assertFalse(controller.cadastrar(nomeCargoVazio, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
-				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		Integer cbo2002Zero = 0;
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002Zero, cbo94, horasMes,
-				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		Integer cbo94Zero = 0;
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94Zero, horasMes,
-				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		Integer horasMesZero = 0;
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMesZero,
-				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-
-		Integer grauInstrucaoZero = 0;
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
-				grauInstrucaoZero, experienciaMinima, atribuicoes, status, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		String experienciaMinimaVazio = "";
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
-				grauInstrucao, experienciaMinimaVazio, atribuicoes, status, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		String atribuicoesVazio = "";
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
-				grauInstrucao, experienciaMinima, atribuicoesVazio, status, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		Boolean statusNulo = null;
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
-				grauInstrucao, experienciaMinima, atribuicoes, statusNulo, idPermissao));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		Integer idPermissaoZero = 0;
-		assertFalse(controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
-				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissaoZero));
-		assertEquals(0, controller.buscarTodos().size());
-		
-		
+	controller.cadastrar(nomeCargoVazio, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
+				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarCbo2002Invalido() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, 0, cbo94, horasMes,
+				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao);
 	}
 
+	@Test(expected = Exception.class)
+	public void testCadastrarCbo94Invalido() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, 0, horasMes,
+				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarHorasMesInvalido() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, 0,
+				grauInstrucao, experienciaMinima, atribuicoes, status, idPermissao);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarGrauInstrucaoInvalido() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
+				0, experienciaMinima, atribuicoes, status, idPermissao);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarExperienciaMinimaInvalida() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
+				grauInstrucao, "", atribuicoes, status, idPermissao);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarAtribuicoesInvalido() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
+				grauInstrucao, experienciaMinima, "", status, idPermissao);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarStatusInvalido() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
+				grauInstrucao, experienciaMinima, atribuicoes, null, idPermissao);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testCadastrarIdPermissaoInvalido() throws Exception {
+		controller.cadastrar(nomeCargo, dataCadastro, dataUltimaRevisao, cbo2002, cbo94, horasMes,
+				grauInstrucao, experienciaMinima, atribuicoes, status, 0);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
