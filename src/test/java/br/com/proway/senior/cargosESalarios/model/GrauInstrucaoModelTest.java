376875a676abef63f174dfd1fe6841122d6d7fc2
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-18 10:15:01 -0300

    Commit inicial

---
+++ src/test/java/br/com/proway/senior/cargosESalarios/model/GrauInstrucaoModelTest.java
@@ -0,0 +1,21 @@
+package br.com.proway.senior.cargosESalarios.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Testes da Classe GrauInstrucaoModel {@link GrauInstrucaoModel}.
+ * 
+ * @author Sarah Brito <b>sarah.brito@senior.com.br</b> - Sprint 5
+ */
+public class GrauInstrucaoModelTest {
+
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
