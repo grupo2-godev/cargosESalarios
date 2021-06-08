02a0cb338bb6a9763f729c193c1de55718d15f5a
Lucas Ivan <lucas.ivan.medeiros@gmail.com>
2021-06-08 11:09:29 -0300

    atualizacao do HTML e criacao dos componentes no app

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ html/script.js
@@ -0,0 +1,6 @@
+let a  = ["a", 1, 1.1, false, null, [1, "s", null]]
+
+
+for(let i = 0; i <= 15; i++){
+    console.log(a[i])
+}
